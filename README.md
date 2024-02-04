edbfe3e68942af1328dbc2a6f03c63efd502909f
Michel Fernandes <michelpf@gmail.com>
2024-02-03 21:56:14 -0300

    importacao inicial

---
+++ README.md
@@ -0,0 +1,23 @@
+[![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://laptopricing.streamlit.app)
+
+# Aplicação para Preparação de Foto para Documentos
+
+Esta aplicação (frontend) fornece uma camada de entrada para avaliação de fotos como medida de segurança para on-boarding de aplicações bancárias ou afins.
+
+Os seguintes requisitos precisam estar presentes para a foto ser aceita:
+
+* Uma única foto na imagem
+* Ausência de emoções
+
+Após a análise de emoções, a imagem deve ser recortada somente para a região onde se encontra a face.
+
+## Componentes utilizados:
+
+* Frontend: [Streamlit](https://streamlit.io/)
+* Serverless backend (AWS Lambda)[https://aws.amazon.com/pt/lambda/]
+
+## Uso
+
+Esta aplicação está configurada no Streamlit Cloud de tal forma que qualquer modificação na branch ```master``` será lançada uma nova atualização.
+
+Para realizar depuração local utilizando o VSCode, utilze o ```launch.json``` que possui as configurações necessárias.
---
+++ app.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import requests
+import base64
+import json
+from PIL import Image
+
+"""
+# Cognitive Platforms
+
+## Detecção de Foto para Documentos
+
+Este aplicativo visa detectar face para emissão de documentos, 
+validando as seguintes condições e características:
+
+* Existência de face
+* Verificação de neutralidade via análise sentimental
+
+Estando as duas condições validadas, a foto está aprovada para emissão de documento.
+
+"""
+
+
+
+foto = st.camera_input("Tire sua foto. Procure centralizar seu rosto.")
+
+if foto:
+
+    with st.spinner('Validando condições...'):
+        
+        bytes_data = foto.getvalue()
+        img_base64 = base64.b64encode(bytes_data).decode('utf-8')
+ 
+        payload = {
+            "image_base64": img_base64
+        }
+
+        payload = json.dumps(payload)
+
+        # URL da sua API Gateway
+        url = "https://u2dspwvgdq6dwi6qj26lyl5y5e0hcqzq.lambda-url.us-east-1.on.aws"
+
+        response = requests.post(url, json=payload)
+        
+
+        # Verifique se a solicitação foi bem-sucedida
+        if response.status_code == 200:
+            response_data = json.loads(response.text)
+
+            imagem = Image.open(foto)
+
+            largura_total, altura_total = imagem.size
+
+            # Define as coordenadas do retângulo de corte com base nas medidas fornecidas
+            left = int(response_data["boundingBox"]["Left"] * largura_total)
+            top = int(response_data["boundingBox"]["Top"] * altura_total)
+            width = int(response_data["boundingBox"]["Width"] * largura_total)
+            height = int(response_data["boundingBox"]["Height"] * altura_total)
+
+            # Recorta a região de interesse (ROI) da imagem
+            regiao_recortada = imagem.crop((left, top, left+width, top+height))
+
+            st.image(regiao_recortada)
+
+            st.success('Foto validada com sucesso! Emissão autorizada.', icon="✅")
+        else:
+            st.error(response.text, icon="🚨")
+
+
+
+       
